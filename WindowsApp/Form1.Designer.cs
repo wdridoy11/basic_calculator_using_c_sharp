266548c97ede33de2c80d3191843f4d3afdc6b59
USER <USER@DESKTOP-0CMEG9N>
2024-01-28 18:39:12 +0600

    Add project files.

---
+++ WindowsApp/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.inputField1 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.add = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.inputField2 = new System.Windows.Forms.TextBox();
+            this.output = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(943, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // inputField1
+            // 
+            this.inputField1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.inputField1.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputField1.Location = new System.Drawing.Point(12, 12);
+            this.inputField1.Multiline = true;
+            this.inputField1.Name = "inputField1";
+            this.inputField1.Size = new System.Drawing.Size(477, 48);
+            this.inputField1.TabIndex = 1;
+            // 
+            // add
+            // 
+            this.add.Font = new System.Drawing.Font("Times New Roman", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.add.Location = new System.Drawing.Point(405, 66);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(39, 32);
+            this.add.TabIndex = 5;
+            this.add.Text = "+";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(450, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(39, 33);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "-";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(495, 66);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(39, 32);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "*";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Times New Roman", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(540, 66);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(39, 34);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // inputField2
+            // 
+            this.inputField2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.inputField2.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.inputField2.Location = new System.Drawing.Point(495, 12);
+            this.inputField2.Multiline = true;
+            this.inputField2.Name = "inputField2";
+            this.inputField2.Size = new System.Drawing.Size(436, 48);
+            this.inputField2.TabIndex = 10;
+            // 
+            // output
+            // 
+            this.output.Font = new System.Drawing.Font("Times New Roman", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.output.Location = new System.Drawing.Point(183, 186);
+            this.output.Multiline = true;
+            this.output.Name = "output";
+            this.output.Size = new System.Drawing.Size(465, 45);
+            this.output.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(943, 621);
+            this.Controls.Add(this.output);
+            this.Controls.Add(this.inputField2);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.add);
+            this.Controls.Add(this.inputField1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.TextBox inputField1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox inputField2;
+        private System.Windows.Forms.TextBox output;
+    }
+}
+
---
+++ WindowsApp/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void myNameToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello Bangladesh!");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //int a, b, c;
+            //a =Convert.ToInt16(firstInput.Text);
+            //b =Convert.ToInt16(secoundInput.Text);
+            //c = a * b;
+            //sumOutpu.Text = c.ToString();
+        }
+
+        //sum
+        private void add_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int input1, input2, sum;
+                if (!string.IsNullOrEmpty(inputField1.Text)) ;
+                input1 = Convert.ToInt16(inputField1.Text);
+
+                if (!string.IsNullOrEmpty(inputField2.Text)) ;
+                input2 = Convert.ToInt16(inputField2.Text);
+
+                sum = input1 + input2;
+                output.Text = sum.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Please put any number");
+            }
+        }
+
+        // sub
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int input1, input2, sub;
+                if (!string.IsNullOrEmpty(inputField1.Text)) ;
+                input1 = Convert.ToInt16(inputField1.Text);
+
+                if (!string.IsNullOrEmpty(inputField2.Text)) ;
+                input2 = Convert.ToInt16(inputField2.Text);
+
+                sub = input1 - input2;
+                output.Text = sub.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Please put any number");
+            }
+        }
+        //multiplication
+        private void button3_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int input1, input2, multiplication;
+                if (!string.IsNullOrEmpty(inputField1.Text)) ;
+                input1 = Convert.ToInt16(inputField1.Text);
+
+                if (!string.IsNullOrEmpty(inputField2.Text)) ;
+                input2 = Convert.ToInt16(inputField2.Text);
+
+                multiplication = input1 * input2;
+                output.Text = multiplication.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Please put any number");
+            }
+        }
+        //division
+        private void button4_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int input1, input2, division;
+                if (!string.IsNullOrEmpty(inputField1.Text)) ;
+                input1 = Convert.ToInt16(inputField1.Text);
+
+                if (!string.IsNullOrEmpty(inputField2.Text)) ;
+                input2 = Convert.ToInt16(inputField2.Text);
+
+                division = input1 / input2;
+                output.Text = division.ToString();
+            }
+            catch
+            {
+                MessageBox.Show("Please put any number");
+            }
+        }
+    }
+}
